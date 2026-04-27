c5837bc5c3aa08899b978abe4cdc6c0effbe0bcd
monavixx <dperelygin0@gmail.com>
2026-04-28 00:17:58 +0300

    Add AuthController and UsersController for user authentication and retrieval

---
+++ BookingSystem.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using BookingSystem.Api.Common;
+using BookingSystem.Application.Features.Users.Commands.SignUp;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.Api.Controllers;
+
+[Route("api/auth")]
+public class AuthController(IMediator mediator) : ApiController(mediator)
+{
+    [HttpPost("sign-up")]
+    public async Task<IActionResult> SignUp([FromBody] SignUpCommand command)
+    {
+        var result = await Mediator.Send(command);
+        if (result.IsSuccess)
+            return CreatedAtAction(nameof(UsersController.GetCurrentUser),
+                nameof(UsersController).Replace("Controller", ""), null, result.Value);
+        return HandleErrors(result);
+    }
+}
---
+++ BookingSystem.Api/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using BookingSystem.Api.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.Api.Controllers;
+
+[Route("api/users")]
+public class UsersController(IMediator mediator) : ApiController(mediator)
+{
+    [HttpGet("me")]
+    public async Task<IActionResult> GetCurrentUser()
+    {
+        //TODO:
+        return Ok();
+    }
+}
