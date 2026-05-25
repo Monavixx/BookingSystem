38c01c46af8ea5e780ce9b8b636f2cba73166740
monavixx <dperelygin0@gmail.com>
2026-05-25 21:08:12 +0300

    Add validation for restaurant creation command

---
+++ BookingSystem.Application/Features/Restaurants/Commands/CreateRestaurant/CreateRestaurantValidation.cs
@@ -0,0 +1,36 @@
+using FluentValidation;
+
+namespace BookingSystem.Application.Features.Restaurants.Commands.CreateRestaurant;
+
+public class CreateRestaurantValidation : AbstractValidator<CreateRestaurantCommand>
+{
+    public CreateRestaurantValidation()
+    {
+        RuleFor(x => x.Address)
+            .NotNull()
+            .ChildRules(v =>
+                {
+                    v.RuleFor(x => x.Country)
+                        .NotEmpty();
+                    v.RuleFor(x => x.State)
+                        .Must(x => x?.Length is not 0)
+                        .WithMessage("State can be either null or not empty");
+                    v.RuleFor(x => x.City)
+                        .Must(x => x?.Length is not 0)
+                        .WithMessage("City can be either null or not empty");
+                    v.RuleFor(x => x.Street)
+                        .Must(x => x?.Length is not 0)
+                        .WithMessage("Street can be either null or not empty");
+                    v.RuleFor(x => x.HouseNumber)
+                        .Must(x => x?.Length is not 0)
+                        .WithMessage("HouseNumber can be either null or not empty");
+                    v.RuleFor(x => x.ApartmentNumber)
+                        .Must(x => x?.Length is not 0)
+                        .WithMessage("ApartmentNumber can be either null or not empty");
+                    v.RuleFor(x => x.ZipCode)
+                        .Must(x => x?.Length is not 0)
+                        .WithMessage("ZipCode can be either null or not empty");
+                }
+            );
+    }
+}
