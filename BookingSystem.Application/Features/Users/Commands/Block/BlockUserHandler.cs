8e861129b6b407d229c3eea6311ed7b095d58a0b
monavixx <dperelygin0@gmail.com>
2026-07-29 22:34:54 +0300

    nothing, just formatting

--- BookingSystem.Application/Features/Users/Commands/Block/BlockUserHandler.cs
+++ BookingSystem.Application/Features/Users/Commands/Block/BlockUserHandler.cs
@@ -9,15 +9,15 @@ namespace BookingSystem.Application.Features.Users.Commands.Block;
 public class BlockUserHandler(AppDbContext dbContext, TimeProvider timeProvider)
     : IRequestHandler<BlockUserCommand, Result>
 {
-    public async Task<Result> Handle(BlockUserCommand request, CancellationToken cancellationToken)
-    {
-        var user = await dbContext.Users.FindAsync([new UserId(request.UserId)], cancellationToken);
-        if (user is null) return UserErrors.NotFound;
-        
-        var res = user.Block(timeProvider, request.Duration);
-        if(res.IsFailed) return res;
-        
-        await dbContext.SaveChangesAsync(cancellationToken);
-        return Result.Ok();
-    }
+	public async Task<Result> Handle(BlockUserCommand request, CancellationToken cancellationToken)
+	{
+		var user = await dbContext.Users.FindAsync([new UserId(request.UserId)], cancellationToken);
+		if (user is null) return UserErrors.NotFound;
+
+		var res = user.Block(timeProvider, request.Duration);
+		if (res.IsFailed) return res;
+
+		await dbContext.SaveChangesAsync(cancellationToken);
+		return Result.Ok();
+	}
 }
